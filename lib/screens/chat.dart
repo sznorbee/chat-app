9908d5e45b0361d01ff233764fd877426cb3720c
Norbert Szekeres <norbert.szekeres@gmail.com>
2024-05-02 12:13:29 +0200

    add firebase functions

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,10 +1,29 @@
 import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setUpPushNotification() async {
+    final fcm = FirebaseMessaging.instance;
+    await FirebaseMessaging.instance.requestPermission(provisional: true);
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setUpPushNotification();
+  }
 
   @override
   Widget build(BuildContext context) {
